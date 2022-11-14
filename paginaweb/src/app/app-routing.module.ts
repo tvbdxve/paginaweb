da9fdc963f9d41d0e06a5c76c2d6d31d591c1050
unknown <dfsv2015@gmail.com>
2022-11-14 12:01:29 -0500

    clase de navegacion

--- paginaweb/src/app/app-routing.module.ts
+++ paginaweb/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Componente1Component } from './components/componente1/componente1.component';
+import { Componente2Component } from './components/componente2/componente2.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', component:Componente1Component
+  },
+  {
+    path:'web2', component:Componente2Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- paginaweb/src/app/app.module.ts
+++ paginaweb/src/app/app.module.ts
@@ -4,9 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ComponentsComponent } from './components/components.component';
+import { Componente2Component } from './components/componente2/componente2.component';
+import { Componente1Component } from './components/componente1/componente1.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Componente1Component,
+    ComponentsComponent,
+    Componente2Component
   ],
   imports: [
     BrowserModule,
